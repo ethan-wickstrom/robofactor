981c25dc1c4bc514053fc5fe2416cd205761fca1
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2025-09-05 11:02:12 -0500

    refactor: reorder beartype checks for clarity and update __all__ type annotation

--- src/robofactor/__init__.py
+++ src/robofactor/__init__.py
@@ -5,12 +5,10 @@ with contextlib.suppress(Exception):
 
     from beartype import BeartypeConf
     from beartype.claw import beartype_all, beartype_this_package
-
-    beartype_this_package()
-
-    if os.environ.get("ROBOFACTOR_BEARTYPE_ALL", "1") == "1":
+    if os.environ.get("ROBOFACTOR_BEARTYPE_THIS_PACKAGE", "0") == "1":
+        beartype_this_package()
+    if os.environ.get("ROBOFACTOR_BEARTYPE_ALL", "0") == "1":
         beartype_all(conf=BeartypeConf(violation_type=UserWarning))
-
 from .data import examples, models
 
-__all__ = ["examples", "models"]
+__all__: list[str] = ["examples", "models"]
